1b18ed25f80d1e30cdca7cdbc1c2f757115256d8
razvan.puha <puha.razvan@gmail.com>
2024-02-27 14:18:37 +0200

    Add Dockerfile and docker-compose

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func main() {
 	InitDB()
 
 	r := gin.Default()
+	// For healthcheck
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.POST(fmt.Sprintf("%s/auth/register", baseUriPath), RegisterUser)
 	r.POST(fmt.Sprintf("%s/auth/login", baseUriPath), LoginUser)
 	r.GET(fmt.Sprintf("%s/auth/currentUser", baseUriPath), VerifyToken, GetCurrentUserDetails)
